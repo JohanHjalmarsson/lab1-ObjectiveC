c90850fefe84b514a6247a5fee5bccb07841852a
JohanHjalmarsson <johan.g.hjalmarsson@gmail.com>
2018-01-15 14:07:58 +0100

    Initial Commit

---
+++ Lab1JohanHjalmarsson/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lab1JohanHjalmarsson
+//
+//  Created by Johan Hjalmarsson on 2018-01-15.
+//  Copyright © 2018 Johan Hjalmarsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Lab1JohanHjalmarsson/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lab1JohanHjalmarsson
+//
+//  Created by Johan Hjalmarsson on 2018-01-15.
+//  Copyright © 2018 Johan Hjalmarsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
